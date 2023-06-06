8ed5269386cb799b3447de04d3e172a7f4c78050
gdobs07 <n11273445@qut.edu.au>
2023-06-07 01:44:00 +1000

    added routes to views

--- a3_group7/ConcertCove/views.py
+++ a3_group7/ConcertCove/views.py
@@ -3,6 +3,24 @@ from flask import Blueprint, render_template
 bp = Blueprint('main', __name__)
 
 
+# Will add more links to all pages and need to add neccessary code to each route
 @bp.route('/')
+@bp.route('/index')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    return render_template('index.html', title='Home')
+
+@bp.route('/create')
+def create():
+    return render_template('create.html', title='Create')
+
+@bp.route('/show')
+def show():
+    return render_template('show.html', title='Show')
+
+@bp.route('/404error')
+def error_404():
+    return render_template('404error.html', title='404error')
+
+@bp.route('/500error')
+def error_500():
+    return render_template('500error.html', title='500error')
